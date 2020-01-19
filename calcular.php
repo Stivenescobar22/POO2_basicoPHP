93b73c71bec4757b94b2716b4271b8cb52df360e
stiven escobar galeano <estibenes@gmail.com>
2020-01-19 13:24:27 -0500

    se implementa POO para realizar la serie fibonacci de un numero

---
+++ calcular.php
@@ -0,0 +1,31 @@
+<?php 
+
+   if(isset($_POST["btncalcular"])){
+        
+    require_once 'Fibonacci.php';
+
+    $fibonacci = new Fibonacci();
+    
+    $valor = $_POST["txtvalor"];
+
+    $fibonacci->setValorserie($valor);
+
+    $fibonacci->fibonacciHacerMientras();
+
+
+    echo "<br>";
+
+    $fibonacci->fibonacciMientras();
+
+    echo "<br>";
+
+    $fibonacci->fibonacciPara();
+
+
+   }else{
+
+    echo "No me hackees";
+   }
+
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<HTML>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset= utf-8">
+        <title>Ejercicio basico Poo 2</title>
+    
+    </head>
+    <body>
+    
+      <p>hacer un programa que muestre la interaccion fibonacci de cualquier valor, con POO</p>
+
+      <form name="form1" method="POST" action="calcular.php">
+     
+      <p>Ingresa valor : <input type="text" name="txtvalor"></p>
+      <p><input type="submit" name="btncalcular" value="calcular"></p>
+
+  </form>
+     
+      
+    </body>
+      
+</html>
